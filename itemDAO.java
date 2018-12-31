fc77b9916331318bea63de6cdf4d542110f4663d
sinhasandeep <46277215+sinhasandeep@users.noreply.github.com>
2018-12-31 20:47:49 +0800

    Add files via upload

---
+++ itemDAO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ItemDAOTest;
+
+/**
+ *
+ * @author ssinha
+ */
+public class itemDAO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create a connection to database
+        // get result set and assign particular fields valuse to DTO object
+        // use a DTO object
+        
+        
+    }
+    
+}
